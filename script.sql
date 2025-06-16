ff56927a51b628551e4f3057ddcca9fe743c562e
Gustavo Henrique <gustavosousasantos54@gmail.com>
2025-06-16 17:35:48 -0300

    feat(ap11_ex): criacao de blocos anonimos e resolucao da atividade

--- script.sql
+++ script.sql
@@ -85,4 +85,60 @@ BEGIN
     INSERT INTO tb_log_operacao (nome_procedimento) VALUES ('sp_cadastrar_varios_clientes');
 END;
 $$
+-----------------------------------------------------------------------------------------------------------------------------------
+-- 1.6 Para cada procedimento criado, escreva um bloco anônimo que o coloca em execução.
+
+DO $$
+DECLARE
+    v_cod_cliente_teste;
+BEGIN
+    SELECT cod_cliente INTO v_cod_cliente_teste FROM tb_cliente WHERE nome LIKE 'Ana' LIMIT 1;
+    IF v_cod_cliente_teste IS NOT NULL THEN
+        CALL sp_exibir_total_pedidos_cliente(v_cod_cliente_teste);
+    ELSE
+        RAISE NOTICE 'Cliente "Ana" não encontrado. Por favor, insira um cliente e crie pedidos para ele.';
+    END IF;
+END;
+$$
+----------------------------------------------------------------------------------------------------------
  
+DO $$
+DECLARE
+    v_cod_cliente_teste;
+    v_total_pedidos_out
+BEGIN
+    SELECT cod_cliente INTO v_cod_cliente_teste FROM tb_cliente WHERE nome LIKE 'Ana' LIMIT 1;
+    IF v_cod_cliente_teste IS NOT NULL THEN
+        CALL sp_exibir_total_pedidos_cliente_out(v_cod_cliente_teste, v_total_pedidos_out);
+        RAISE NOTICE 'O cliente com código % tem um total de % pedidos em OUT.', v_cod_cliente_teste, v_total_pedidos_out;
+    ELSE
+        RAISE NOTICE 'Cliente "Ana" não encontrado. Por favor, insira um cliente e crie pedidos para ele.';
+    END IF;
+END;
+$$
+----------------------------------------------------------------------------------------------------------
+ 
+DO $$
+DECLARE
+    v_cod_cliente_inout INT;
+BEGIN
+    SELECT cod_cliente INTO v_cod_cliente_inout FROM tb_cliente WHERE nome LIKE 'Ana' LIMIT 1;
+    IF v_cod_cliente_inout IS NOT NULL THEN
+        RAISE NOTICE 'Código do cliente antes da chamada INOUT: %', v_cod_cliente_inout;
+        CALL sp_obter_total_pedidos_cliente_inout(v_cod_cliente_inout);
+        RAISE NOTICE 'O código do cliente (agora total de pedidos) após INOUT é: %', v_cod_cliente_inout;
+    ELSE
+        RAISE NOTICE 'Cliente "Ana" não encontrado. Por favor, insira um cliente e crie pedidos para ele.';
+    END IF;
+END;
+$$
+----------------------------------------------------------------------------------------------------------
+ 
+DO $$
+DECLARE
+    v_mensagem_saida VARCHAR(500);
+BEGIN
+    CALL sp_cadastrar_varios_clientes(v_mensagem_saida, 'Roberto', 'Mariana', 'Carlos');
+    RAISE NOTICE '%', v_mensagem_saida;
+END;
+$$
