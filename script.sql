e783b301ce88bda94fe567da4215800e2e93adde
Gustavo Henrique <gustavosousasantos54@gmail.com>
2025-06-16 17:33:35 -0300

    feat(ap11_ex): resolucao exercicio 1.4

--- script.sql
+++ script.sql
@@ -39,3 +39,21 @@ BEGIN
     INSERT INTO tb_log_operacao (nome_procedimento) VALUES ('sp_exibir_total_pedidos_cliente_out');
 END;
 $$
+
+-----------------------------------------------------------------------------------------------------------------------------------
+-- 1.4 Adicione um procedimento ao sistema do restaurante. Ele deve
+-- - Receber um parâmetro de entrada e saída (INOUT)
+-- - Na entrada, o parâmetro possui o código de um cliente
+-- - Na saída, o parâmetro deve possuir o número total de pedidos realizados pelo cliente
+CREATE OR REPLACE PROCEDURE sp_exibir_total_pedidos_cliente_inout(INOUT p_cod_cliente_e_total_pedido)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_cod_cliente := p_cod_cliente_e_total_pedido;
+BEGIN
+    SELECT COUNT(*) FROM tb_pedido
+    WHERE cod_cliente = v_cod_cliente
+    INTO p_cod_cliente_e_total_pedido;
+    INSERT INTO tb_log_operacao (nome_procedimento) VALUES ('sp_exibir_total_pedidos_cliente_inout');
+END;
+$$
